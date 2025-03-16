c38d45c0bc3e0e9f8a6c98ce5b7e5bfb1c9c7ae2
StivenJ714 <jsvargasco@unadvirtual.edu.co>
2025-03-16 12:04:34 -0500

    Trabajos en clase

---
+++ main.py
@@ -0,0 +1,18 @@
+import FUNCIONESS as fun
+from FUNCIONESS import mayoria_de_edad, mayoria_de_edad2
+import tkinter as tkr
+from tkinter import simpledialog
+
+base = tkr.Tk()
+base.title ("")
+
+nombre = simpledialog.askstring ("Nombre", "Ingrese su nombre: ")
+edad = simpledialog.askinteger ("Edad", "Ingrese su edad: ")
+
+mensaje = mayoria_de_edad(nombre, edad)
+
+usar_label = tkr.Label(base, text=mensaje, font=("Arial",20))
+usar_label.pack(pady=30)
+
+
+base.mainloop()
